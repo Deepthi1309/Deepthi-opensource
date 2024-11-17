823aa0f768fd9beeaaaa0417647fdd0cd7a57be4
Deepthi1309 <deepthiginikunta03@gmail.com>
2024-11-17 22:31:18 +0530

    Create Season_game.py

---
+++ visaa_prep/Season_game.py
@@ -0,0 +1,11 @@
+a=int(input())
+if a<1 or a>12:
+    print("Invalid")
+elif 3<=a<=5:
+    print("Spring")
+elif 6<=a<=8:
+    print("Summer")
+elif 9<=a<=11:
+    print("Autumn")
+else:
+    print("Winter")
